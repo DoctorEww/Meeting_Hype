d62ce0f67576882d1f90ba93b0ff908a2de22b22
DoctorEww <dgriess084@gmail.com>
2021-03-04 11:37:55 -0800

    even more hype

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 #This bot inducts you to the hall of fame if you successfully hack REDACTED during the March 5th 2021 meeting. 
 # Author DoctorEww
 
@@ -24,17 +26,17 @@ async def on_ready():
                     messages = await channel.history(limit=None).flatten()
                     if (len(messages) == 0):
                         await channel.send(f"Beep Boop Boop-Erh, I mean hello humans! Do not be alarmed; I already ate. I bring you NULLify's first-ever hall-of-fame challenge. For this challenge, you must access my creator's IP through Omegle. Then, you must compromise the vulnerable web server hosted there; once you get a shell, pivot to another user account. There, you will find my source code and be immortalized in the hall-of-fame.")
-                    for message in messages:
+                    for message in messages: #Check if its already been solved!
                         if (str(message.author) == "The Legendary Omegle Hack#8779"):
                             if (str(member.id) in message.content):
                                 print("You have already solved this challenge!")
-                                break
-                        await channel.send(f"<@{member.id}> has successfully hacked their way through and completed NULLify's first ever hall of fame challenge!")
-                        print("Congrats! Welcome to the hall of fame!")
+                                print("All done! You can stop the bot now... it does nothing else </3")
+                                return
+                    await channel.send(f"<@{member.id}> has successfully hacked their way through and completed NULLify's first ever hall of fame challenge!")
+                    print("Congrats! Welcome to the hall of fame!")
                     found = 1
                     break
             if (found == 0):
                 print("User not found... Try again! It's not your nickname")
     print("All done! You can stop the bot now... it does nothing else </3")
-    sys.exit(0)
 client.run(TOKEN)
